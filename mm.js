729f3824474ec3db66aabb00307b1f277b95b223
DCWorks <pshonature@gmail.com>
2020-12-02 08:50:20 +0900

    first on start010

--- mm.js
+++ mm.js
@@ -1,41 +1,50 @@
-let currentInput = null;
+let phoneNumber = null;
 
-function goFocus(target = currentInput) {
-    if (target == null) {
-        target = document.getElementById("numFirst");
-    }
-    currentInput = target;
-    target.focus();
-}
 
 function touchNumber() {
     currentInput = this;
 }
 
 function touchButton() {
-    goFocus();
-    currentInput.value += this.value;
-    switch (currentInput.id) {
-        case "numFirst":
-            if (currentInput.value.length >= 3) {
-                goFocus(document.getElementById("numSecond"));
-            }
+    this.blur();
+    if (phoneNumber.value.length >= 13)
+        return;
+    phoneNumber.value += this.value;
+    switch (phoneNumber.value.length) {
+        case 3:
+        case 8:
+            phoneNumber.value += "-";
             break;
-        case "numSecond":
-            if (currentInput.value.length >= 4) {
-                goFocus(document.getElementById("numThird"));
-            }
+    }
+}
+
+function touchDelete() {
+    let length = phoneNumber.value.length;
+    switch (length) {
+        case 0:
+            return;
+        case 9:
+        case 4:
+            phoneNumber.value = phoneNumber.value.slice(0, length - 1);
             break;
-        case "numThird":
-            if (currentInput.value.length >= 4) {
-                goFocus(document.getElementById("numFirst"));
-            }
-            break;
-    } //end of switch
-    goFocus();
+    }
+    phoneNumber.value = phoneNumber.value.slice(0, phoneNumber.value.length - 1);
+}
+
+function touchClear() {
+    phoneNumber.value = "";
+}
+
+function keyDown() {
+    alert(event.keyCode);
 }
 
 window.onload = function() {
+    phoneNumber = document.getElementById("phoneNumber");
+    $(phoneNumber).focus(function() { $(phoneNumber).blur() });
     $("input").click(touchNumber);
-    $("button").click(touchButton);
+    $(".btnNumber").click(touchButton);
+    $(".btnDelete").click(touchDelete);
+    $(".btnClear").click(touchClear);
+    // $(document).keydown(keyDown);
 };
