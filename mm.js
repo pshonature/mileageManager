85a6f07f72a2fdff5d1fedd6cacb5500f902ea18
DCWorks <pshonature@gmail.com>
2020-12-03 09:38:39 +0900

    Revert "저장 후 메시지 변경 타이머 설정"
    This reverts commit 2f6fba74755ff64cd9373269b16ab93b99584287.

--- mm.js
+++ mm.js
@@ -146,9 +146,6 @@ function touchSave() {
     $(".btnClear").click();
     onoff(".save, .btnNumber, .btnDelete, .btnClear", "off");
     $("#amount").val("저장되었습니다.");
-    setTimeout(function() {
-        putMessage("적립할 금액을 [check]하여 주세요.");
-    }, 5000);
 }
 
 function putMessage(msg) {
@@ -184,7 +181,6 @@ window.onload = function() {
     $("#start010").click();
 
     onoff(".save, .btnNumber, .btnDelete, .btnClear", "off");
-    putMessage("적립할 금액을 [check]하여 주세요.");
 
 };
 window.onunload = function() {
