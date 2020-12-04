ea284537ff6def59b28876b2a393f92e14e8febe
Pongsik, Ho <pshonature@gmail.com>
2020-12-04 16:10:38 +0900

    사소한 버튼 on-off 오류 수정 완료

--- mm.js
+++ mm.js
@@ -39,13 +39,20 @@ function getMileage() {
 // findOrNewMBook: MBOOK에서 휴대폰 번호와 일치하는 MBook을 찾는다. 
 //  없으면 새 MBook 생성 저장 후 반환
 function findOrNewMBook(phone) {
+    let mb = searchMBook(phone);
+    if (mb == null) {
+        mb = new MBook(phone);
+        MBOOK.mb.push(mb);
+    }
+    return mb;
+}
+
+function searchMBook(phone) {
     for (let amb of MBOOK.mb) {
         if (amb.phone === phone)
             return amb;
     }
-    let newMB = new MBook(phone);
-    MBOOK.mb.push(newMB);
-    return newMB;
+    return null;
 }
 //=========================================
 function toCommaNumber(n) {
@@ -56,10 +63,19 @@ function touchNumber() {
     currentInput = this;
 }
 
+function precheckCurrentPhone() {
+    let mb = searchMBook(phoneNumber.value);
+    if (mb) { //적립 이력이 있는 번호
+        putMessage(mBookReport(mb));
+    } else {
+        putMessage("첫 거래 감사합니다. 단골 고객이 되어주세요 ^^.");
+    }
+}
+
 function touchButton() {
     this.blur();
     if (phoneNumber.value.length >= 13) {
-        onoff(".save", "on");
+        // onoff(".save", "on");
         return;
     }
     phoneNumber.value += this.value;
@@ -72,7 +88,9 @@ function touchButton() {
     if (phoneNumber.value.length < 13) {
         onoff(".save", "off");
         onoff(".btnNumber", "on");
+        putMessage("적립할 휴대폰 번호를 입력해 주세요");
     } else {
+        precheckCurrentPhone();
         onoff(".save", "on");
         onoff(".btnNumber", "off");
     }
@@ -87,6 +105,7 @@ function touchStart010() {
 }
 
 function touchDelete() {
+    putMessage("적립할 휴대폰 번호를 입력해 주세요");
     onoff(".btnNumber", "on");
     onoff(".save", "off");
     let length = phoneNumber.value.length;
@@ -109,6 +128,7 @@ function touchClear() {
     onoff(".btnNumber", "on");
     onoff(".save, #start010", "off");
     phoneNumber.value = "010-";
+    putMessage("적립할 휴대폰 번호를 입력해 주세요.");
 }
 
 function keyDown() {
@@ -130,6 +150,7 @@ function touchCheck() {
     onoff(".btnNumber, .btnDelete, .btnClear, .cancel", "on");
     onoff(".amount, .save, #start010", "off");
     putMessage("적립할 휴대폰 번호를 입력해 주세요.");
+    $(".btnClear").click();
 }
 
 function touchInput() {
@@ -140,6 +161,8 @@ function afterSaving() {
     $("#amount").val("적립할 금액 확인 =>");
     putMessage("적립할 금액을 먼저 확인[Check] 해주세요.");
     onoff(".amount", "on");
+    onoff(".btnNumber, .save, .cancel, .btnDelete, .btnClear", "off");
+    phoneNumber.value = "";
 }
 
 function touchSave() {
@@ -148,7 +171,7 @@ function touchSave() {
     putMessage(mBookReport(fmb));
 
 
-    $(".btnClear").click();
+    // $(".btnClear").click();
     onoff(".save, .amount, .cancel, .btnNumber, .btnDelete, .btnClear", "off");
     $("#amount").val("저장되었습니다.");
 
@@ -159,12 +182,12 @@ function touchCancel() {
     putMessage("적립을 취소하였습니다.");
     onoff(".cancel", "off");
     $(phoneNumber).val("");
+    onoff(".save, .cancel, .btnNumber, .btnDelete, .btnClear", "off");
     setTimeout(function() {
         afterSaving();
         $("#amount").val("적립할 금액 확인 =>");
         putMessage("적립할 금액을 먼저 확인[Check] 해주세요.");
-        $("#start010").click();
-        onoff(".save, .cancel, .btnNumber, .btnDelete, .btnClear", "off");
+        phoneNumber.value = "010-";
         onoff(".amount", "on");
     }, 3000);
 }
